d50c2f9b060e84b4adcfe4f39d4c98d4df4a826b
Tarcísio Ferreira Sassara <sassara.tarcisio@gmail.com>
2024-07-05 21:26:40 -0300

    Update init.tf

--- services/catalog/init.tf
+++ services/catalog/init.tf
@@ -14,16 +14,3 @@ module "catalog_init_lambda" {
   ]
   global_layer = var.global_layer
 }
-
-# resource "aws_lambda_invocation" "init_catalog" {
-#   function_name = aws_lambda_function.lambda_function_test.function_name
-
-#   input = jsonencode({
-#     key1 = "value1"
-#     key2 = "value2"
-#   })
-# }
-
-# output "result_entry" {
-#   value = jsondecode(aws_lambda_invocation.example.result)["key1"]
-# }
